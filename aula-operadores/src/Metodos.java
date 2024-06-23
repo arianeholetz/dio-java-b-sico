f2f6532fc02d0015db7b83c8cb813c03e96550e4
Ari <arianeholetz@gmail.com>
2024-06-23 15:51:20 -0300

    #alteracoes-aula-tres

---
+++ aula-operadores/src/Metodos.java
@@ -0,0 +1,8 @@
+public class Metodos {
+
+    // Método para somar dois números inteiros
+    public double somar(int num1, int num2) {
+        // Retorna a soma dos dois números
+        return num1 + num2;
+    }
+}
--- aula-operadores/src/Operadores.java
+++ aula-operadores/src/Operadores.java
@@ -1,4 +1,4 @@
-public class App {
+public class Operadores {
     public static void main(String[] args) {
         int numero1 = 1;
         int numero2 = 2;
