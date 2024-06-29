506c311395682393a48c9f3f1e5b69f260ead182
Ari <arianeholetz@gmail.com>
2024-06-29 15:16:20 -0300

    criado-projeto-conta-terminal

--- contaBanco/ContaBanco/src/ContaTerminal.java
+++ contaBanco/ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Coleta dos dados do usuário
+        System.out.print("Digite seu nome completo: ");
+        String nomeSobrenome = scanner.nextLine();
+        
+        System.out.print("Digite sua agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.print("Digite o número da conta: ");
+        String numeroConta = scanner.nextLine();
+        
+        System.out.print("Digite seu saldo: ");
+        double saldo = 0.0;
+        
+        if (scanner.hasNextDouble()) {
+            saldo = scanner.nextDouble();
+        } else {
+            throw new IllegalArgumentException("Formato de saldo inválido. Por favor digite um número válido, com centesimais separados com vírgula. ");
+        }
+
+        scanner.nextLine();
+        
+        // Mensagem formatada
+        String mensagem = String.format("Olá %s, obrigada por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque.",
+                                         nomeSobrenome, agencia, numeroConta, saldo);
+        
+        // Exibição da mensagem
+        System.out.println(mensagem);
+        
+        scanner.close();
+    }
 }
