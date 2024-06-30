63c256598279c7092e5e1790e678b7639343fee9
Ari <arianeholetz@gmail.com>
2024-06-30 15:56:42 -0300

    alterando-aula-projeto

---
+++ controle-de-candidatos/src/MelhorDeCinco.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.ThreadLocalRandom;
+static double valorPretendido() {
+    return ThreadLocalRandom.current().nextDouble(1800,2200);
+}
+
+public class MelhorDeCinco {
+    
+}
